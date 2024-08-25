452be71a47842c6ef42449e47e13f88b94f9d158
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-26 08:47:56 +0900

    基礎文法05

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 `use strict`;
 
-console.log(100);
-console.log(-10);
-console.log(2.5);
+/*
+JavaScriptにおける数値の表現
+@taguchi
+*/
 
-console.log(1.2e4);
-console.log(1.2e-4);
+// console.log(100);
+// console.log(-10);
+// console.log(2.5);
+
+//大きな数値、小さな数値
+console.log(1.2e4); // 1.2×10000 = 12000
+console.log(1.2e-4); // 1.2×0.0001 = 0.00012
