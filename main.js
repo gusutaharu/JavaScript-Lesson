2a7c2c0cc66a7921361b599c6dd7dc6de246c818
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-27 08:27:22 +0900

    基礎文法12

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 `use strict`;
 
-console.log("It is a pen");
-console.log("It`s a pen");
-console.log('It\'s a pen');
-console.log("It`s \na \npen");
+// My name is Taro Yamada, call me Taro!
+
+const fname = 'Taro';
+const lname = 'Yamada';
+
+// console.log("My name is" + fname + " " + lname + ", call me " + fname + "!");
+console.log(`My name is ${fname} ${lname}, call me ${fname}!`)
