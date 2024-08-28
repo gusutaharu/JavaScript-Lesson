ebcd7d2fae3d842278c78c1e3740da188ed75803
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-28 09:55:23 +0900

    基礎文法22

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 `use strict`;
 
-// const command = Number(prompt('Menu 1, 2, 3 or 0 exit'));
-// console.log(`Menu ${command} processed`);
 
-let command = Number(prompt('Menu 1, 2, 3 or 0 exit'));
+// let command = Number(prompt('Menu 1, 2, 3 or 0 exit'));
 
-while (command !== 0) {
-  console.log(`Menu ${command} processed`);
+// while (command !== 0) {
+//   console.log(`Menu ${command} processed`);
+//   command = Number(prompt('Menu 1, 2, 3 or 0 exit'));
+// }
+
+let command;
+
+do {
   command = Number(prompt('Menu 1, 2, 3 or 0 exit'));
-}
+  if(command === 0){
+    console.log('Exited');
+  }else {
+    console.log(`Menu ${command} processed`)
+  }
+} while ( command !== 0);
