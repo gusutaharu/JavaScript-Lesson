da65044010b2089a1c0c9db43c5c0398d20b01c2
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-29 22:53:09 +0900

    関数5

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 `use strict`;
 
 {
-  // function sum(a,b){
-  //   return a + b;
+  function sum(a,b){
+    return a + b;
   // }
-  function sum(a,b) {
-    console.log(a + b);
+  // function sum(a,b) {
+    // console.log(a + b);
+    // return undefined
   }
-  sum(300,700);
+  console.log(sum(300,700)*3);
 }
