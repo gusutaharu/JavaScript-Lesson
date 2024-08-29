5959dc1b0adbd8c92b681ef94daf12509502828c
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-29 23:10:50 +0900

    関数8

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 `use strict`;
 
 {
-  function calculateTotal(price,amount,rate= 1.1){
-    // if (amount >= 100) {
-    //   return price * amount;
-    // } else {
-    //   return price * amount * rate;
-    // }
-    //早期リターン
-    if (amount >= 100) {
-      return price * amount;
-    }
-    return price * amount * rate;
-    }
-
-  console.log(calculateTotal(100, 100));
-  console.log(calculateTotal(1000, 10));
+  function double(num){
+    return num * 2
+  }
+  function triple(num){
+    return num * 3
+  }
+  console.log(double(10));
+  console.log(triple(20));
 }
