b31d75a30884ce25c25529b6de5e9de529a76f2c
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-30 09:16:29 +0900

    データ08

--- main.js
+++ main.js
@@ -7,14 +7,8 @@
     80,
     85,
   ];
-  // console.log(scores[0]);
-  // console.log(scores[1]);
-  // console.log(scores[2]);
-  // console.log(scores[3]);
-
-  scores.push(77,88)
-
-  for (let i = 0; i < scores.length; i++) {
-    console.log(scores[i]);
-  }
+  scores.forEach((score,index) => {
+    // console.log(score)
+    console.log(`${index}: ${score}`);
+  });
 }
