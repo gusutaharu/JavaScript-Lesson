81283cfeabafd33215d8dff5fc15d91f6d12f5be
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-30 09:57:03 +0900

    データ09

--- main.js
+++ main.js
@@ -7,8 +7,14 @@
     80,
     85,
   ];
+
+  let sum = 0;
+  
   scores.forEach((score,index) => {
-    // console.log(score)
+    sum += score;
     console.log(`${index}: ${score}`);
   });
+
+  console.log(`Sum: ${sum}`);
+  console.log(`Average: ${sum / scores.length}`)
 }
