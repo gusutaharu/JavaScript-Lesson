cfdfb8b920311a10e83becb4bbc67a84ac09b570
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-08-31 09:41:57 +0900

    データ16

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 `use strict`;
 
 {
-  const scores = {
-    math: 80,
-    english: 90,
-  };
-  let sum = 0;
+  const scores = [70, 90, 80, 85];
 
-  scores.physics = 70;
-
-  const entries = Object.entries(scores);
-  entries.forEach((prop) => {
-    sum  += prop[1];
-    console.log(`${prop[0]}: ${prop[1]}`);
-  });
-  console.log(`Sum: ${sum}`);
-  console.log(`Average: ${sum / entries.length}`);
+  scores.splice(2, 0, 77, 88);
+  const deleted = scores.splice(3,1);
+  scores.splice(2,2,30);
+  console.log(scores);
+  console.log(deleted);
+  // scores [70,90,30,85]
+  // deleted [88]
 }
