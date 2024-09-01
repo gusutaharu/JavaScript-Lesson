beb9c52724d63a94d40a212a87cba6a373553727
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-01 17:30:20 +0900

    データ17

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 `use strict`;
 
 {
-  const scores = [70, 90, 80, 85];
+  // const names = ['Taro', 'Jiro', 'Saburo']
+  // //Taro|Jiro|Saburo
+  // console.log(names.join(`|`));
+  // console.log(names.join());
+  // console.log(names.join(``));
 
-  scores.splice(2, 0, 77, 88);
-  const deleted = scores.splice(3,1);
-  scores.splice(2,2,30);
-  console.log(scores);
-  console.log(deleted);
-  // scores [70,90,30,85]
-  // deleted [88]
+  const names = 'Taro|Jiro|Saburo';
+  console.log(names.split('|'));
 }
