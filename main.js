7090678159654766f173511bee52025215ae36ef
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-01 23:21:51 +0900

    データ21

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 `use strict`;
 
 {
-  const  scores = [70, 90, 80, 85];
-  // const first = scores[0];
-  // const second = scores[1];
-  // const third = scores[2];
-  // const fourth = scores[3];
-  const [first, second, third, fourth] = scores;
+  let start = 'Tokyo';
+  let goal = 'Osaka';
 
-  console.log(first);
-  console.log(second);
-  console.log(third);
-  console.log(fourth);
+  // start = goal; //start = Osaka
+  // goal = start; //goal = start = Osaka
+
+  // let temp = '';
+  // temp = start;
+  // start = goal;
+  // goal = temp;
+
+  [goal, start] = [start, goal];
+
+  console.log(start);
+  console.log(goal);
 }
