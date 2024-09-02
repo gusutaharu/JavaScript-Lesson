f8ab452c28313a336fb1da3cc05cd1e7f85900ba
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-03 08:23:47 +0900

    DOM編02

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 `use strict`;
 
 {
-  // let num = 10;
-  // const numBackup = num;
-  // num = 99;
-  // console.log(num);
-  // console.log(numBackup);
-  const nums = [10,20,30];
-  const numsBackup = nums;
-  nums[0] = 99;
-  console.log(nums);
-  console.log(numsBackup);
+  document.querySelector('button').addEventListener('click',()=>{console.log('clicked');
+  });
 }
