c2b070e329d2b15f9804c0ce01e23b427ec6c992
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-04 08:43:17 +0900

    DOM編05

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 `use strict`;
 
 {
-  document.querySelector('button').addEventListener('click',()=>{console.log('clicked');
+  document.querySelector('button').addEventListener('click',()=>{
+    // console.log(document.querySelector('p').classList.contains('pink-bg'));
+    // if (document.querySelector('p').classList.contains('pink-bg') === false) {
+    //   document.querySelector('p').classList.add('pink-bg');
+    // } else {
+    //   document.querySelector('p').classList.remove('pink-bg')
+    // }
+    document.querySelector('p').classList.toggle('pink-bg');
   });
 }
