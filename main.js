abd06c5b053d85797f08d66f1d3d4159bcbfbad9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-05 22:12:58 +0900

    DOM編10

--- main.js
+++ main.js
@@ -4,5 +4,12 @@
   document.querySelector('button').addEventListener('click',()=>{
     const liElement = document.createElement('li');
     liElement.textContent = 'Hanako';
+    // document.querySelector('ul').appendChild(liElement);
+    // document.querySelector('ul').insertBefore(liElement,
+    //   document.querySelector('#second')
+    // );
+    if(confirm('Sure?') === true) {
+      document.querySelector('#second').remove()
+    } 
   });
 }
