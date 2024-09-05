fbaa28684db8952ac775f5f9236b735d08400d78
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-05 18:32:13 +0900

    DOM編08

--- main.js
+++ main.js
@@ -2,12 +2,7 @@
 
 {
   document.querySelector('button').addEventListener('click',()=>{
-    document.querySelector('#second').textContent = 'Changed';
-    // document.querySelectorAll('li')[0].textContent = 'Changed';
-    // document.querySelectorAll('li')[1].textContent = 'Changed';
-    // document.querySelectorAll('li')[2].textContent = 'Changed';
-    // document.querySelectorAll('.target').forEach((li)=>{
-    //   li.textContent = 'Changed!'
-    // });
+    const liElement = document.createElement('li');
+    liElement.textContent = 'Hanako';
   });
 }
