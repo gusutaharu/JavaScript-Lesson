d375f871b755cc876ae87728358373c7299c499d
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-07 08:36:01 +0900

    DOM編16

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 `use strict`;
 
 {
-  document.querySelector('input').addEventListener('input',()=>{
-    const pElement = document.querySelector('p');
-    const inputElement = document.querySelector('input');
-
-    // pElement.textContent = inputElement.value
-    pElement.textContent = inputElement.value.length
+  document.querySelector('input').addEventListener('focus',()=>{
+    document.querySelector('p').textContent = 'English only!';
   });
+
+  document.querySelector('input').addEventListener('blur',()=>{
+    document.querySelector('p').textContent = '';
+  });
+
+  document.querySelector('input').focus();
 }
