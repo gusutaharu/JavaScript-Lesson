e9cb3fa9f6006391a77a76fe0d08a196bbf87fa6
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-07 09:09:39 +0900

    DOM編18

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 `use strict`;
 
 {
-  document.addEventListener('keydown',(e)=>{
-    document.querySelector('p').textContent = e.key;
+  document.addEventListener('mousemove',(e)=>{
+    document.querySelector('p').textContent = `X: ${e.clientX} Y: ${e.clientY}`
   });
 }
