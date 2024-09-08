933e3f20dc9f8bf524d17e36d7e83760f08556c7
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-08 16:56:05 +0900

    数値編01

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 `use strict`;
 
 {
-  document.querySelector('button').addEventListener('click',()=>{
-    // document.querySelector('p').style = 'font-size: 24px;'
-    document.querySelector('p').style.fontSize = '24px';
-  });
+const n =  Number(prompt('Number?'));
+console.log(n + 5);
 }
