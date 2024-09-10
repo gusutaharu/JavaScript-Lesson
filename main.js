19d0c1ba07dd4bcd7f70c4653c28323839c0cff9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-10 11:05:31 +0900

    文字列編06

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 `use strict`;
 
 {
-  const emails = [
-    'taro@example.com',
-    'kintaro@example.com',
-    'kojiro@example.com',
-  ];
+  const counts = [6, 12,8,15];
 
-  emails.forEach((email)=>{
-    //console.log(email.replace('@example.com', ''))
-    const items = email.split('@');
-    console.log(items[0])
-  });
+  counts.forEach((count)=>{
+    // let bar = '';
+    // for (let i = 0; i < count; i++){
+    //   bar += '*';
+    // }
+    const bar = '*'.repeat(count);
+    console.log(bar);
+  })
 }
