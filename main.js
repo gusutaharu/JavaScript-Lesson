723e9d688ea7e63fcca9049f3b96cecb5c3f2732
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-10 10:00:34 +0900

    文字列編03

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 `use strict`;
 
 {
-  const string = prompt('Name?');
-  // if (string.toLowerCase() === 'taro'){
-  if (string.toUpperCase().trim() === 'TARO'){
-    console.log('Correct!');
-  } else {
-    console.log('Wrong!');
-  }
+  const emails = [
+    'taro@example.com',
+    'kintaro@example.com',
+    'kojiro@example.com',
+  ];
+
+  emails.forEach((email)=>{
+    // if (email.includes('taro') === true){
+    //   console.log(email);
+    // }
+    // if  (email.indexOf('taro') === 0){
+    //   console.log(email);
+    // }
+    if (email.startsWith('taro') === true){
+      console.log(email);
+    }
+  });
 }
