fc9758ab786f9b28928f5165efbf937418aeeac4
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-12 23:11:30 +0900

    main.js

--- main.js
+++ main.js
@@ -3,7 +3,11 @@
 {
   //2000 4 11
   const d = new Date(2000, 3, 11);
-  // d.setDate(23);
-  d.setDate(d.getDate() + 100);
-  console.log(d.toLocaleString());
+  // const dBackup = d;
+  // const dBackup = new Date(d.getFullYear(), d.getMonth(),d.getDate())
+  const dBackup = new Date(d.getTime());
+  d.setDate(23);
+
+  console.log(d.toLocaleString());//23
+  console.log(dBackup.toLocaleString());//11
 }
