c2f4d8dd04938ea0563a3f43bd381882e19cf909
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-14 08:47:06 +0900

    タイマー編01

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 `use strict`;
 
 {
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const d = new Date(2001, 0, 1)
-
-  const month = months[d.getMonth()];
-  const date = d.getDate();
-  const day = days[d.getDay()];
-  console.log(`${month}, ${date}, ${day}`);
+  console.log(new Date());
+  setInterval(()=>{
+    console.log(new Date());
+  }, 1000);
 }
