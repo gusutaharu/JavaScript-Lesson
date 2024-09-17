ae7ed69aac306858bed758697c7b04bf7721766e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-18 08:39:26 +0900

    クラス編02

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 `use strict`;
 
 {
-//typeof
-console.log(typeof 5);//number
-console.log(typeof 'Hello');//string
-console.log(typeof [5,8]);//object
-console.log(typeof {math: 80, english: 90});//object
-console.log(typeof true);//boolean
-console.log(typeof undefined);//undefined
-console.log(typeof null);//object
+  class User {
+
+  }
+
+  const user1 = new  User();
+  const user2 = new User();
 }
