1fce1cb911cf8a498c1c0ed0b74dcb81f14a50fb
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-18 09:54:49 +0900

    クラス編05

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 `use strict`;
 
 {
-  // class User {
-  //   constructor(nameFromNew, scoreFromNew){
-  //     this.name = nameFromNew;
-  //     this.score = scoreFromNew;
-  //   }
-  // }
   class User {
     constructor(name, score){
       this.name = name;
       this.score = score;
     }
+
+    getUserString(){
+      return `${this.name} ${this.score}`;
+    }
   }
 
-  const user1 = new  User('Taro', 70);
+  const user1 = new User('Taro', 70);
   const user2 = new User('Jiro', 80);
 
-  console.log(user1.name);
-  console.log(user1.score);
-  console.log(user2.name);
-  console.log(user2.score);
+  console.log(user1.getUserString());
+  console.log(user2.getUserString());
 }
