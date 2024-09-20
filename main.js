b003b83c9b04376f7ba6a53d69f5b6d6b0f5087b
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-21 08:33:32 +0900

    クラス編13

--- main.js
+++ main.js
@@ -1,49 +1,21 @@
 `use strict`;
 
 {
-  class Score {
-    constructor(subject, result) {
-      this.subject = subject;
-      this.result = result;
-    }
-    
-    getGrade(){
-      return this.result >= 80 ? 'A' : 'B';
-    }
-
-    getScoreString(){
-      return `${this.subject} ${this.result} ${this.getGrade()}`;
-    }
-  }
-
-  class MathScore extends Score {
-    constructor(result) {
-      super('Math', result)
-    }
-    getGrade(){
-      return this.result >= 50 ? 'A' : 'B';
-    }
-  }
-
-  class EnglishScore extends Score {
-    constructor(result) {
-      super('English', result)
-    }
-  }
-
   class User {
+    name;
+    score;
     constructor(name, score){
       this.name = name;
       this.score = score;
     }
 
     getUserString(){
-      return `${this.name} ${this.score.getScoreString()}`;
+      return `${this.name} ${this.score}`;
     }
   }
 
-  const user1 = new User('Taro', new MathScore(70));
-  const user2 = new User('Jiro', new EnglishScore(80));
+  const user1 = new User('Taro',70);
+  const user2 = new User('Jiro',80);
 
   console.log(user1.getUserString());
   console.log(user2.getUserString());
