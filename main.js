7190a90b1ee8513bdd2c27a24ea64ee5c450aa61
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-23 08:20:08 +0900

    fetch API編02

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
 `use strict`;
 
 {
-  class User {
-    name;
-    #score;
-    constructor(name, score){
-      this.name = name;
-      this.#score = score;
-    }
-
-    get score() {
-      return this.#score;
-    }
-
-    #isValueValid(newValue) {
-      return newValue < 0 || newValue > 100 ? false : true;
-    }
-
-    set score(newValue) {
-      if (this.#isValueValid(newValue) === false ){
-        console.log('Invalid value!');
-        return;
-      }
-      this.#score = newValue;
-    }
+  function showHeader(){
+    console.log('Header');
   }
 
-  const user = new User('Taro',70);
-  user.score = 9999;
-  console.log(user.score);
-  // console.log(user.#isValueValid(50));
+  function showUsers(){
+    console.log('Users');
+  }
+
+  function showFooter(){
+    console.log('Footer');
+  }
+
+  showHeader();
+  showUsers();
+  showFooter();
 }
