fc9326ace7044c1d517afbc613101fc6628fe58e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-22 15:12:40 +0900

    クラス編16

--- main.js
+++ main.js
@@ -4,29 +4,20 @@
   class User {
     name;
     score;
-    static count = 0;
     constructor(name, score){
       this.name = name;
       this.score = score;
-      User.count++;
     }
 
-    getUserString(){
-      return `${this.name} ${this.score}`;
+    get score() {
+
     }
 
-    static getUserCountString(){
-      return `${User.count} instance(s) created.`;
+    set score() {
     }
   }
 
-  // let count = 0;
-  const user1 = new User('Taro',70);
-  // count++;
-  const user2 = new User('Jiro',80);
-  // count++;
-  console.log(User.getUserCountString());
-
-  console.log(user1.getUserString());
-  console.log(user2.getUserString());
+  const user = new User('Taro',70);
+  user.score = 100;
+  console.log(user.score);
 }
