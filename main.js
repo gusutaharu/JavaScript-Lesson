d839535359208c94920fb8a624a8e75c7f059400
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-23 10:08:36 +0900

    fetch API編08

--- main.js
+++ main.js
@@ -5,16 +5,30 @@
     console.log('Header');
   }
 
-  const showUsers = async () => {
-    try {
-      const response = await fetch('https://dotinstall.github.io/setup/fetchapi/users.json');
-      const users = await response.json();
+  // const showUsers = async () => {
+  //   try {
+  //     const response = await fetch('https://dotinstall.github.io/setup/fetchapi/users.json');
+  //     const users = await response.json();
+  //     console.log(users);
+  //   } catch(err) {
+  //     console.log('Something went wrong getting user data');
+  //     console.log('Error log:' + err);
+  //   }
+  // };
+
+  function showUsers() {
+    fetch('https://dotinstall.github.io/setup/fetchapi/users.json').then((response) => {
+      response.json();
       console.log(users);
-    } catch(err) {
+    })
+    .then((users)=>{
+      console.log(users)
+    })
+    .catch((err)=>{
       console.log('Something went wrong getting user data');
       console.log('Error log:' + err);
-    }
-  };
+    })
+  }
 
   function showFooter(){
     console.log('Footer');
