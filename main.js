d121a420d6d2f210159c31297639434205d2209a
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-23 09:19:17 +0900

    fetch API編06

--- main.js
+++ main.js
@@ -6,9 +6,14 @@
   }
 
   async function showUsers(){
-    const response = await fetch('https://dotinstall.github.io/setup/fetchapi/users.json');
-    const users = await response.json();
-    console.log(users);
+    try {
+      const response = await fetch('https://dotinstall.github.io/setup/fetchapi/invalid-users.json');
+      const users = await response.json();
+      console.log(users);
+    } catch(err) {
+      console.log('Something went wrong getting user data');
+      console.log('Error log:' + err);
+    }
   }
 
   function showFooter(){
